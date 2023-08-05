6b3302d195edeffb4e0a2515d9a73c4ff60e4fa7
robotian <robotian@gmail.com>
2023-08-04 22:09:11 -0400

    change colcon workspace to ros2_ws

--- setup.bash
+++ setup.bash
@@ -9,19 +9,19 @@ export PATH=$HOME/gazebo_tools/ardupilot/build/sitl/bin:$PATH
 export PATH=$HOME/gazebo_tools/ardupilot/Tools/autotest:$PATH
 
 # Add results of colcon build
-source $HOME/ws_orca4/install/setup.bash
+source $HOME/ros2_ws/install/setup.bash
 
 # Add ardupilot_gazebo plugin
-export GZ_SIM_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:$Z_SIM_SYSTEM_PLUGIN_PATH
+export GZ_SIM_SYSTEM_PLUGIN_PATH=$HOME/gazebo_tools/ardupilot_gazebo/build:$Z_SIM_SYSTEM_PLUGIN_PATH
 
 # Optional: add ardupilot_gazebo models and worlds
-export GZ_SIM_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:$GZ_SIM_RESOURCE_PATH
+export GZ_SIM_RESOURCE_PATH=$HOME/gazebo_tools/ardupilot_gazebo/models:$HOME/gazebo_tools/ardupilot_gazebo/worlds:$GZ_SIM_RESOURCE_PATH
 
 # Add bluerov2_ignition models and worlds
-export GZ_SIM_RESOURCE_PATH=$HOME/ws_orca4/src/bluerov2_ignition/models:$HOME/ws_orca4/src/bluerov2_ignition/worlds:$GZ_SIM_RESOURCE_PATH
+export GZ_SIM_RESOURCE_PATH=$HOME/ros2_ws/src/bluerov2_ignition/models:$HOME/ros2_ws/src/bluerov2_ignition/worlds:$GZ_SIM_RESOURCE_PATH
 
 # Add orca4 models and worlds
-export GZ_SIM_RESOURCE_PATH=$HOME/ws_orca4/src/orca4/orca_description/models:$HOME/ws_orca4/src/orca4/orca_description/worlds:$GZ_SIM_RESOURCE_PATH
+export GZ_SIM_RESOURCE_PATH=$HOME/ros2_ws/src/orca4/orca_description/models:$HOME/ros2_ws/src/orca4/orca_description/worlds:$GZ_SIM_RESOURCE_PATH
 
 # Build ros_gz on the humble branch for Gazebo Garden
 export GZ_VERSION=garden
